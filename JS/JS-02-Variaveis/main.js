8a445bdc0d26bf21204c2a1d2b476394e65af2da
Leonardo2574 <leonardoosaraiva0@gmail.com>
2024-05-08 21:44:26 -0300

    variaveis

---
+++ JS/JS-02-Variaveis/main.js
@@ -0,0 +1,32 @@
+// objeto console
+console.log("Ola Mundo!")
+//quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+
+//variaveis
+//quero criar um espaço na memoria do computador chamado _leonardo e dizer que esse espaço na memoria ira receber o valor
+var _leonardo = 8
+var _leonardo = "Messi" //redeclarar uma variavel
+console.log(_leonardo)
+
+let letsgo = 4 //não posso redeclarar
+letsgo = 6
+console.log(letsgo)
+
+console.clear()
+
+const PI = 3.1415
+console.log(PI)
+
+
+let NAN = 0/0
+console.log(NAN)
+
+console.clear()
+
+let palmeiras = "Avanti palestra"
+console.log("Avanti" + "palestra")
+//concatenar é possivel com strings ou variaveis
+
+var num1 = 1
+var num2 = 5
+console.log(num1 == num2)
