9440b48e409840a129d7c6353ae89f9e5834f025
Leonardo2574 <leonardoosaraiva0@gmail.com>
2024-05-16 19:53:17 -0300

    exercicio string

---
+++ JS/JS-03-String/ExercicioCasa/main.js
@@ -0,0 +1,6 @@
+var string = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+console.log(string[19]);
+console.log(string.length);
+console.log(string.toUpperCase())
+console.log(string.substring(48))
--- JS/JS-05-Repeticao/ExercicioCasa/main.js
+++ JS/JS-05-Repeticao/ExercicioCasa/main.js
@@ -1,6 +1,6 @@
-var tabuada = 4
+var tabuada = 3
 
-if(tabuada === 2 || tabuada === 4 ){
+if(tabuada === 3  ){
     console.log(`tabuada do numero ${tabuada}:`)
 
     for ( var numero = 1; numero <=10; numero++){
