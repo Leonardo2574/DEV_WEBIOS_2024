109562f23188c3d076700462cedf26fc80983084
Leonardo2574 <leonardoosaraiva0@gmail.com>
2024-05-15 21:41:12 -0300

    repetição

---
+++ JS/JS-05-Repeticao/main.js
@@ -0,0 +1,34 @@
+//while
+//Enquanto 0 < 10 vou apresentar a mensagem "0 ainda é menor que 10"
+
+var contador = 0
+
+while(contador <= 10){
+    console.log(contador + " ainda é menor que 10")
+    contador++;
+}
+
+var contador = 0
+
+do{
+    debugger
+    console.log(contador + " ainda é menor que 10")
+    contador++;
+}while(contador < 10)
+
+
+if{
+    for(){
+
+    }
+}else{
+    while(){
+
+    }
+}
+
+
+
+for(contador = 0; contador < 10; contador++;){
+    console.log(contador + "ainda é menor que 10")
+}
